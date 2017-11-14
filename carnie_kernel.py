2c83f0fbf63438bfe16a17d95d6d6f94de12c7fb
Alex Hall <alexhall@eecs.berkeley.edu>
2017-11-13 22:43:44 -0500

    uncommented stuff from debugging

--- carnie_kernel.py
+++ carnie_kernel.py
@@ -5,14 +5,14 @@ from scannerpy.stdlib import parsers, pykernel
 class CarnieKernel:
 
     def execute(self, frame, bboxes):
-        # RudeCarnie(model_dir='/n/scanner/models/rude-carnie')
+        rc = RudeCarnie(model_dir='/n/scanner/models/rude-carnie')
         bboxes = parsers.deserialize(bboxes)
         for bb in bboxes:
             face_img = frame[int(h * bb.y1):int(h * bb.y2),
                              int(w * bb.x1):int(w * bb.x2)]
             face_img = cv2.resize(face_img, (out_size, out_size))
             face_img = facenet.prewhiten(face_img)
-            gender = self.rc.get_gender(faces, single_look=True)
+            gender = rc.get_gender(faces, single_look=True)
 
         return [result]
 
